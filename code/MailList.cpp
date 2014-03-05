9c484de285ff4e03c685ff8d9d544b68d2a5f2c2
trikitrok <trikitrok@gmail.com>
2014-03-05 16:42:56 +0100

    Refactor: moved the empty string case to a guard clause

--- code/MailList.cpp
+++ code/MailList.cpp
@@ -11,9 +11,10 @@ MailList::~MailList() {
 std::vector<std::string> MailList::getMails(const std::string & emailAddressesString) const {
   std::vector<std::string> emailAddressesList;
 
-  if (!emailAddressesString.empty()) {
-    emailAddressesList.push_back(emailAddressesString);
+  if (emailAddressesString.empty()) {
+    return emailAddressesList;
   }
 
+  emailAddressesList.push_back(emailAddressesString);
   return emailAddressesList;
 }

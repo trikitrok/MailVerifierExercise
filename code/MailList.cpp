2651c4f2d15da1e663a5788e462e08b514f5e3bc
trikitrok <trikitrok@gmail.com>
2014-03-05 16:51:24 +0100

    It includes two valid email addresses separated by a comma in the email addresses list

--- code/MailList.cpp
+++ code/MailList.cpp
@@ -15,6 +15,18 @@ std::vector<std::string> MailList::getMails(const std::string & emailAddressesSt
     return emailAddressesList;
   }
 
-  emailAddressesList.push_back(emailAddressesString);
+  auto commaPosition = emailAddressesString.find(",");
+
+  if (commaPosition == std::string::npos) {
+    emailAddressesList.push_back(emailAddressesString);
+    return emailAddressesList;
+  }
+
+  auto emailAddress = emailAddressesString.substr(0, commaPosition);
+  emailAddressesList.push_back(emailAddress);
+
+  auto restOfEmailAddressesList = emailAddressesString.substr(commaPosition + 1);
+  emailAddressesList.push_back(restOfEmailAddressesList);
+  
   return emailAddressesList;
 }
--- tests/MailListTest.cpp
+++ tests/MailListTest.cpp
@@ -5,12 +5,18 @@
 
 using namespace ::testing;
 
-TEST(AMailList, ReturnsAnEmptyEmailAddressesListWhenPassedAnEmptyString) {
+TEST(AMailList, ReturnsAnEmptyListWhenPassedAnEmptyString) {
   MailList mailList(new MailVerifier);
   ASSERT_THAT(mailList.getMails(""), IsEmpty());
 }
 
-TEST(AMailList, IncludesAValidEmailAddressInTheEmailAddressesList) {
+TEST(AMailList, IncludesAValidEmailAddress) {
   MailList mailList(new MailVerifier);
   ASSERT_THAT(mailList.getMails("hola@hola.com"), ElementsAre("hola@hola.com"));
+}
+
+TEST(AMailList, IncludesTwoValidEmailAddressesSeparatedByCommas) {
+  MailList mailList(new MailVerifier);
+  ASSERT_THAT(mailList.getMails("h@h.com,x@x.com"), 
+    ElementsAre("h@h.com", "x@x.com"));
 }
